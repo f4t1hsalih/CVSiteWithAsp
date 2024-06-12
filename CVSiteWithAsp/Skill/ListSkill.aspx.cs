2ca1abf04a9a54852bdf9453e847826cf2a8e334
f4t1hsalih <f4t1hsalih@gmail.com>
2024-06-12 13:01:16 +0300

    Lecture 32 - Yetenekler ve Yetenek Listesi

---
+++ CVSiteWithAsp/Skill/ListSkill.aspx.cs
@@ -0,0 +1,12 @@
+﻿using DataSetTableAdapters;
+using System;
+
+public partial class Skill_ListSkill : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        tbl_skillsTableAdapter skillsTableAdapter = new tbl_skillsTableAdapter();
+        Repeater1.DataSource = skillsTableAdapter.GetSkills();
+        Repeater1.DataBind();
+    }
+}
